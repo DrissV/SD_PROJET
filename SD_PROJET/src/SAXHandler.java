1d9e379fdeef5f0e23b9b92e7793241d99872f07
DrissV <driss.vandenheede.techinfo@gmail.com>
2018-03-09 11:03:23 +0100

    Add files via upload

--- SD_PROJET/src/SAXHandler.java
+++ SD_PROJET/src/SAXHandler.java
@@ -1,9 +1,11 @@
-import java.util.ArrayList;
-import java.util.List;
-
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class SAXHandler extends DefaultHandler {
 
@@ -11,20 +13,32 @@ public class SAXHandler extends DefaultHandler {
 	private Airport airport;
 	private List<Airport> airports;
 	private double longitude, latitude;
-	private boolean okLatitude, okLongitude;
-	
+	private boolean okLatitude, okLongitude, okAirline;
+	private Airline airline;
+	private List<Airline> airlines;
+	private Route route;
+	private List<Route> routes;
+	private Map<String, Airport> iataAirport;
+	private Map<String, Airline> iataAirlines;
+
 	public SAXHandler() {
+		airlines = new ArrayList<Airline>();
 		airports = new ArrayList<Airport>();
+		routes = new ArrayList<Route>();
+		iataAirport = new HashMap<String, Airport>();
+		iataAirlines = new HashMap<String, Airline>();
+		graph = new Graph();
 	}
-	
+
 	public Graph getGraph() {
 		return graph;
 	}
-	
+
 	@Override
 	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-		if ("airport".equalsIgnoreCase(qName)) { 
-			airport = new Airport (attributes.getValue("iata"), attributes.getValue("name"), attributes.getValue("city"), attributes.getValue("country"));
+		if ("airport".equalsIgnoreCase(qName)) {
+			airport = new Airport(attributes.getValue("iata"), attributes.getValue("name"), attributes.getValue("city"),
+					attributes.getValue("country"));
 		}
 		if ("longitude".equalsIgnoreCase(qName)) {
 			okLongitude = true;
@@ -32,17 +46,34 @@ public class SAXHandler extends DefaultHandler {
 		if ("latitude".equalsIgnoreCase(qName)) {
 			okLatitude = true;
 		}
+		if ("airline".equalsIgnoreCase(qName)) {
+			airline = new Airline(attributes.getValue("iata"), attributes.getValue("country"));
+			okAirline = true;
+		}
+		if ("route".equalsIgnoreCase(qName)) {
+			route = new Route(iataAirlines.get(attributes.getValue("airline")),
+					iataAirport.get(attributes.getValue("source")),
+					iataAirport.get(attributes.getValue("destination")));
+		}
 	}
-	
+
 	@Override
 	public void endElement(String uri, String localName, String qName) throws SAXException {
 		if ("airport".equalsIgnoreCase(qName)) {
 			airport.setLattitude(latitude);
 			airport.setLongitude(longitude);
 			airports.add(airport);
+			iataAirport.put(airport.getIata(), airport);
+		}
+		if ("airline".equalsIgnoreCase(qName)) {
+			airlines.add(airline);
+			iataAirlines.put(airline.getIata(), airline);
+		}
+		if ("route".equalsIgnoreCase(qName)) {
+			routes.add(route);
 		}
 	}
-	
+
 	@Override
 	public void characters(char[] ch, int start, int length) throws SAXException {
 		if (okLongitude) {
@@ -53,6 +84,9 @@ public class SAXHandler extends DefaultHandler {
 			latitude = Double.parseDouble(new String(ch, start, length));
 			okLatitude = false;
 		}
+		if (okAirline) {
+			airline.setName(new String(ch, start, length));
+			okAirline = false;
+		}
 	}
-
 }
