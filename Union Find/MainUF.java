a5e702121768e20aa5b3dfeb66132d60f2356d6f
DrissV <driss.vandenheede.techinfo@gmail.com>
2018-05-27 22:09:13 +0200

    Delete MainUF.java

--- Union Find/MainUF.java
+++
@@ -1,9 +0,0 @@
-package UnionFind;
-
-public class MainUF {
-  public static void main(String[] args) {
-    UnionFind uf = new UnionFind(1000);
-    uf.union(10, 100);
-    System.out.println(uf.find(100));
-  }
-}
